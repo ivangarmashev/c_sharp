e0d6447e588bbf8a1ae71594f16c5cdb12a35042
Ivan <ivan.garmashev@gmail.com>
2021-03-14 23:46:54 +0300

    #

---
+++ first/NumberChange.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace first
+{
+    public class NumberChange
+    {
+        public int x;
+
+        public int FindMax(int arg1, int arg2)
+        {
+            int result;
+            if (arg1 > arg2)
+            {
+                result = arg1;
+            }
+            else if (arg2 > arg1)
+            {
+                result = arg2;
+            }
+            else
+            {
+                result = 0;
+            }
+            this.x = 100;
+            return result;
+        }
+    }
+}
---
+++ first/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace first
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            //string x = Console.ReadLine();
+            //int y = Convert.ToInt32(x);
+            //Console.WriteLine($"Hello World!{x} ");
+            int result;
+            NumberChange num = new NumberChange();
+            Console.WriteLine(num.x);
+
+            result = num.FindMax(13, 132);
+            Console.WriteLine(result);
+            Console.WriteLine(num.x);
+        }
+    }
+}
