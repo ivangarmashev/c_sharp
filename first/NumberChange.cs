f0f293111f5056618f3b03df9a72426d57579782
Ivan <ivan.garmashev@gmail.com>
2021-03-14 23:55:17 +0300

    #

--- first/NumberChange.cs
+++ first/NumberChange.cs
@@ -21,7 +21,7 @@ namespace first
             {
                 result = 0;
             }
-            this.x = 100 * 2;
+            this.x = 100 * 3;
             return result;
         }
     }
